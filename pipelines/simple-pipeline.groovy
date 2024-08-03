8fa99dac14276141c16f13cfbabc2e178bb61bf7
Avik Chakraborty <avikjis27@gmail.com>
2024-08-03 18:00:10 +0000

    Added more directives

--- pipelines/simple-pipeline.groovy
+++ pipelines/simple-pipeline.groovy
@@ -1,5 +1,37 @@
+/*
+https://www.jenkins.io/doc/book/pipeline/syntax/#stages
+Sections: (A Section can contain one or more  Directives)
+    agent
+    post : always, changed, fixed, regression, aborted, failure, success, unstable, unsuccessful, and cleanup
+    stages
+    steps
+Directives:
+    environment
+    options
+    parameters
+    triggers
+    Jenkins cron syntax
+    stage
+    tools
+    input
+    when
+*/
+
+
+
+
+
 pipeline {
     agent any
+    triggers {
+        cron('H */4 * * 1-5')
+    }
+    tools {
+        maven 'apache-maven-3.0.1' // Should be configured before hand in  Manage Jenkins → Tools.
+    }
+    environment { 
+        CC = 'clang'
+    }
     parameters {
             string(name: 'BRANCH', defaultValue: 'main', description: 'Branch to checkout')
             booleanParam(name: 'PUBLISH', defaultValue: false, description: 'Do you want to publish docker image?')
@@ -10,6 +42,17 @@ pipeline {
     }
     stages {
         stage ("Update job description"){
+            environment { 
+                CC = 'clang'
+            }
+            input {
+                message "Should we continue?"
+                ok "Yes, we should."
+                /*submitter "alice,bob"*/ //Defaults to allowing any user.
+                parameters {
+                    string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                }
+            }
             steps{
                 script {
                     currentBuild.displayName = "#${env.BUILD_ID}-${params.BRANCH}"
@@ -69,6 +112,11 @@ pipeline {
                 sh "docker push ${params.DOCKER_REPO}:${params.DOCKER_TAG}"
             }
             
+        }
+    }
+    post { 
+        always { 
+            echo 'I will always say Hello again!'
         }
     }
 }
